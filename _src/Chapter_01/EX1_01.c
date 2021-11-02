234cb39659a688a0df2a11ce9461cbe982ed3408
Your Name <apoorvabhat80@gmail.com>
2021-11-02 07:03:07 +0530

    add exercise 1

---
+++ _src/Chapter_01/EX1_01.c
@@ -0,0 +1,6 @@
+/*Run the ``hello, world'' program on your system. Experiment with leaving out parts of the program, to see what error messages you get.*/
+#include <stdio.h>
+main()
+{
+	printf("Hello World\n");
+}
