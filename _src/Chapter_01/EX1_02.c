e710170a01b0df298b1904d47a41bf5135709a6e
Your Name <apoorvabhat80@gmail.com>
2021-11-02 07:21:14 +0530

    add exercise 2

---
+++ _src/Chapter_01/EX1_02.c
@@ -0,0 +1,6 @@
+/*‘\a’ is an ASCII BEL character it is a terminal feature it produces a “beep” sound or a notification sound simultaneously displaying a terminal or command prompt(windows) */
+#include <stdio.h>
+main()
+{
+	printf("Hello World\a");  /* gives beep sound*/     
+}
